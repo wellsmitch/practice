f18b758cbb54b3e0c659d2632b309073ed263332
songyubin <1970083855@qq.com>
2019-10-10 11:11:34 +0800

    整理

---
+++ 模块化/sa.js
@@ -0,0 +1,12 @@
+(function (root, factory) {
+    debugger
+    if (typeof define === "function" && define.amd) {
+        define(["libName"], factory);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = factory(require("libName"));
+    } else {
+        root.returnExports = factory(root.libName);
+    }
+}(this, function (b) {
+    // b = 2233;
+}));
